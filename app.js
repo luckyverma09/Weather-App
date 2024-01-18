78f9e582976f578bd962c839d7def539b82a3220
Lucky Verma <idonthaveemail1af@gmail.com>
2024-01-18 19:47:54 +0530

    functionality completed

---
+++ app.js
@@ -0,0 +1,51 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+
+search.addEventListener("click", () => {
+  const apiKey = "e5508a6d2f2be961c96c46dff122bb38";
+  const city = document.querySelector(".search-box input").value;
+
+  if (city == "") {
+    return;
+  }
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`).then( response => response.json()).then(json => {
+    const image = document.querySelector('.weather img');
+    const temperature = document.querySelector('.weather .temp');
+    const description = document.querySelector('.weather .desc');
+    const humidity = document.querySelector('.weather-details .humidity span');
+    const wind = document.querySelector('.weather-details .info-wind span');
+
+    switch (json.weather[0].main) {
+        case 'Clear':
+            image.src = 'images/clear.png';
+            break;
+        case 'Clouds':
+            image.src = 'images/cloud.png';
+            break;
+        case 'Mist':
+            image.src = 'images/mist.png';
+            break;
+        case 'Rain':
+            image.src = 'images/rain.png';
+            break;
+        case 'Snow':
+            image.src = 'images/snow.png';
+            break;
+        case 'Haze':
+            image.src = 'images/mist.png';
+            break;
+        default:
+            image.src = 'images/cloud.png';
+    }
+
+    temperature.innerHTML = `${parseInt(json.main.temp)} <span>°C</span>`;
+    description.innerHTML = `${json.weather[0].description}`;
+    humidity.innerHTML = `${json.main.humidity}`;
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+
+  })
+});
